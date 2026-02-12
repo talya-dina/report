753b20943d0c1c1c733e3ff2a45664bf31ad8b42
talya-dina <talya@ofirsec.com>
2026-02-12 21:24:15 +0200

    Add files via upload

--- taskpane.js
+++ taskpane.js
@@ -9,63 +9,31 @@ Office.onReady((info) => {
 
 function reportEmail() {
   const statusElement = document.getElementById("status-message");
+  
   if (statusElement) {
-    statusElement.innerHTML = "<p style='color: #2b579a;'>מעבד דיווח...</p>";
+    statusElement.innerHTML = "<p style='color: #2b579a;'>מכין את הדיווח...</p>";
   }
 
-  // מקבלים את ה-ID של המייל הנוכחי
-  const itemId = Office.context.mailbox.item.itemId;
-
-  // יצירת בקשת ה-XML לשליחה כקובץ מצורף (EWS)
-  const request = 
-    `<?xml version="1.0" encoding="utf-8"?>
-    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                   xmlns:m="http://schemas.microsoft.com/microsoft/exchange/services/2006/messages"
-                   xmlns:t="http://schemas.microsoft.com/microsoft/exchange/services/2006/types"
-                   xmlns:soap="http://schemas.xml-schema.org/soap/envelope/">
-      <soap:Header>
-        <t:RequestServerVersion Version="Exchange2013" />
-      </soap:Header>
-      <soap:Body>
-        <m:CreateItem MessageDisposition="SendOnly">
-          <m:Items>
-            <t:Message>
-              <t:Subject>דיווח על מייל חשוד - OFIRSEC</t:Subject>
-              <t:Body BodyType="HTML">מצורף מייל שדווח על ידי משתמש כחשוד.</t:Body>
-              <t:Attachments>
-                <t:ItemAttachment>
-                  <t:Name>SuspiciousEmail.eml</t:Name>
-                  <t:Item>
-                    <t:ItemId Id="${itemId}" />
-                  </t:Item>
-                </t:ItemAttachment>
-              </t:Attachments>
-              <t:ToRecipients>
-                <t:Mailbox>
-                  <t:EmailAddress>Info@ofirsec.co.il</t:EmailAddress>
-                </t:Mailbox>
-              </t:ToRecipients>
-            </t:Message>
-          </m:Items>
-        </m:CreateItem>
-      </soap:Body>
-    </soap:Envelope>`;
-
-  // שליחת הבקשה לשרת
-  Office.context.mailbox.makeEwsRequestAsync(request, (asyncResult) => {
-    if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-      if (statusElement) {
-        statusElement.innerHTML = `
-          <div style="text-align:center; margin-top: 20px;">
-            <p style="color:green; font-weight:bold; font-size:18px;">✅ הדיווח נשלח בהצלחה!</p>
-            <p>תודה על ערנותך. צוות האבטחה יבדוק את המייל.</p>
-          </div>`;
+  // פתיחת חלון העברה (Forward) עם המייל המקורי כקובץ מצורף
+  Office.context.mailbox.item.displayForwardForm({
+    'toRecipients': ['Info@ofirsec.co.il'],
+    'htmlBody': 'שלום צוות אבטחת מידע,<br><br>אני מדווח על המייל המצורף כחשוד.<br><br>בברכה,',
+    'attachments': [
+      {
+        'type': Office.MailboxEnums.AttachmentType.Item,
+        'name': 'Suspicious_Email',
+        'itemId': Office.context.mailbox.item.itemId
       }
-    } else {
-      console.error("EWS Error: " + asyncResult.error.message);
-      if (statusElement) {
-        statusElement.innerHTML = "<p style='color:red; margin-top: 20px;'>❌ תקלה בשליחה. אנא דווח ידנית לצוות האבטחה.</p>";
-      }
-    }
+    ]
   });
+
+  // עדכון סטטוס בחלונית לאחר פתיחת החלון
+  if (statusElement) {
+    statusElement.innerHTML = `
+      <div style="text-align:center; margin-top: 20px;">
+        <p style="color: #2b579a; font-weight:bold;">הדיווח מוכן!</p>
+        <p>חלון מייל חדש נפתח. אנא לחץ על <b>'שלח'</b> כדי להעביר אותו לצוות האבטחה.</p>
+      </div>
+    `;
+  }
 }
