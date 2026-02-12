276d8579010f57cabad8e95f314f961957724068
talya-dina <talya@ofirsec.com>
2026-02-12 21:32:42 +0200

    Add files via upload

--- taskpane.js
+++ taskpane.js
@@ -9,31 +9,21 @@ Office.onReady((info) => {
 
 function reportEmail() {
   const statusElement = document.getElementById("status-message");
-  
-  if (statusElement) {
-    statusElement.innerHTML = "<p style='color: #2b579a;'>מכין את הדיווח...</p>";
-  }
+  if (statusElement) statusElement.innerHTML = "<p>מכין דיווח...</p>";
 
-  // פתיחת חלון העברה (Forward) עם המייל המקורי כקובץ מצורף
-  Office.context.mailbox.item.displayForwardForm({
+  // שימוש ב-createForwardAsync - פונקציה הרבה יותר נתמכת
+  Office.context.mailbox.item.createForwardAsync({
     'toRecipients': ['Info@ofirsec.co.il'],
-    'htmlBody': 'שלום צוות אבטחת מידע,<br><br>אני מדווח על המייל המצורף כחשוד.<br><br>בברכה,',
-    'attachments': [
-      {
-        'type': Office.MailboxEnums.AttachmentType.Item,
-        'name': 'Suspicious_Email',
-        'itemId': Office.context.mailbox.item.itemId
-      }
-    ]
+    'htmlBody': 'מצורף דיווח על מייל חשוד.'
+  }, function (asyncResult) {
+    if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+      const forwardItem = asyncResult.value;
+      // פותח את חלון המייל שהוכן
+      forwardItem.display();
+      if (statusElement) statusElement.innerHTML = "<p style='color:green;'>הדיווח מוכן! לחצי על 'שלח' בחלון שנפתח.</p>";
+    } else {
+      console.error(asyncResult.error.message);
+      if (statusElement) statusElement.innerHTML = "<p style='color:red;'>שגיאה ביצירת הדיווח. נסי שוב.</p>";
+    }
   });
-
-  // עדכון סטטוס בחלונית לאחר פתיחת החלון
-  if (statusElement) {
-    statusElement.innerHTML = `
-      <div style="text-align:center; margin-top: 20px;">
-        <p style="color: #2b579a; font-weight:bold;">הדיווח מוכן!</p>
-        <p>חלון מייל חדש נפתח. אנא לחץ על <b>'שלח'</b> כדי להעביר אותו לצוות האבטחה.</p>
-      </div>
-    `;
-  }
 }
