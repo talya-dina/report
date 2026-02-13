d58608cfb53e635eebbf77d8525bb86eb1347010
talya-dina <talya@ofirsec.com>
2026-02-13 12:04:22 +0200

    Add files via upload

--- taskpane.js
+++ taskpane.js
@@ -10,33 +10,43 @@ Office.onReady((info) => {
 function reportEmail() {
   const statusElement = document.getElementById("status-message");
   if (statusElement) {
-    statusElement.innerHTML = "<p style='color: #2b579a;'>מעבד דיווח אוטומטי...</p>";
+    statusElement.innerHTML = "<p style='color: #2b579a;'>מעבד דיווח מאובטח...</p>";
   }
 
-  const itemId = Office.context.mailbox.item.itemId;
+  // מקבלים את ה-ID ומוודאים שהוא בפורמט תקין ל-XML
+  let itemId = Office.context.mailbox.item.itemId;
+  
+  // ניקוי תווים מיוחדים למניעת שגיאת Internal Error
+  itemId = itemId.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 
-  // פקודת XML לשרת לשליחת המייל כקובץ מצורף
+  // פקודת XML לשרת (EWS)
   const ewsRequest = 
     `<?xml version="1.0" encoding="utf-8"?>
     <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
                    xmlns:m="http://schemas.microsoft.com/microsoft/exchange/services/2006/messages" 
                    xmlns:t="http://schemas.microsoft.com/microsoft/exchange/services/2006/types" 
                    xmlns:soap="http://schemas.xml-schema.org/soap/envelope/">
-      <soap:Header><t:RequestServerVersion Version="Exchange2013" /></soap:Header>
+      <soap:Header>
+        <t:RequestServerVersion Version="Exchange2013" />
+      </soap:Header>
       <soap:Body>
         <m:CreateItem MessageDisposition="SendOnly">
           <m:Items>
             <t:Message>
               <t:Subject>דיווח על מייל חשוד - OFIRSEC</t:Subject>
-              <t:Body BodyType="HTML">הודעה זו נוצרה אוטומטית על ידי תוסף הדיווח של OFIRSEC. המייל החשוד מצורף כקובץ.</t:Body>
+              <t:Body BodyType="HTML">מצורף מייל שדווח כחשוד על ידי משתמש קצה באמצעות תוסף OFIRSEC.</t:Body>
               <t:Attachments>
                 <t:ItemAttachment>
                   <t:Name>Suspicious_Email.eml</t:Name>
-                  <t:Item><t:ItemId Id="${itemId}" /></t:Item>
+                  <t:Item>
+                    <t:ItemId Id="${itemId}" />
+                  </t:Item>
                 </t:ItemAttachment>
               </t:Attachments>
               <t:ToRecipients>
-                <t:Mailbox><t:EmailAddress>Info@ofirsec.co.il</t:EmailAddress></t:Mailbox>
+                <t:Mailbox>
+                  <t:EmailAddress>Info@ofirsec.co.il</t:EmailAddress>
+                </t:Mailbox>
               </t:ToRecipients>
             </t:Message>
           </m:Items>
@@ -46,17 +56,25 @@ function reportEmail() {
 
   Office.context.mailbox.makeEwsRequestAsync(ewsRequest, (asyncResult) => {
     if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+      console.log("EWS Success!");
       if (statusElement) {
         statusElement.innerHTML = `
           <div style="text-align:center; margin-top: 20px;">
             <p style="color:green; font-weight:bold; font-size:18px;">✅ הדיווח נשלח בהצלחה!</p>
-            <p>המייל הועבר לצוות האבטחה לבדיקה. תודה!</p>
+            <p>תודה על ערנותך, המייל הועבר לטיפול.</p>
           </div>`;
       }
     } else {
-      console.error(asyncResult.error.message);
+      // הדפסה מפורטת ל-Console כדי שנוכל לאבחן במקרה של תקלה
+      console.error("EWS Failed Status: " + asyncResult.status);
+      console.error("Error Details: ", asyncResult.error);
+      
       if (statusElement) {
-        statusElement.innerHTML = "<p style='color:red;'>שגיאה בשליחה. ייתכן שאין הרשאות מתאימות במניפסט.</p>";
+        statusElement.innerHTML = `
+          <div style="color:red; margin-top: 20px;">
+            <p>❌ חלה שגיאה פנימית בשליחה.</p>
+            <small>קוד שגיאה: ${asyncResult.error.code}</small>
+          </div>`;
       }
     }
   });
